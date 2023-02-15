a9da27d8c6c991376fcbfa99ee126ec3e761bff8
studmanj <bjackso3@hotmail.com>
2023-02-15 16:23:44 -0700

    isRetweeted

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ document.addEventListener('click', function(e){
     if(e.target.dataset.like){
        handleLikeClick(e.target.dataset.like) 
     }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
 })
 
 function handleLikeClick(tweetId){
@@ -24,10 +27,22 @@ function handleLikeClick(tweetId){
     else{
         targetTweetObj.likes++
     }
-
     targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
 
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
 
+    if (targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
     render()
 }
 
