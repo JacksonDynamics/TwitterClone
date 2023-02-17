74eca9e99a73362ced0b9456df3145eaf160aadf
studmanj <bjackso3@hotmail.com>
2023-02-16 18:26:36 -0700

    reply comments

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ document.addEventListener('click', function(e){
     else if(e.target.dataset.retweet){
         handleRetweetClick(e.target.dataset.retweet)
     }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
 })
 
 function handleLikeClick(tweetId){
@@ -46,6 +49,10 @@ function handleRetweetClick(tweetId){
     render()
 }
 
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')   
+}
+
 function getFeedHtml(){ 
     let feedHtml = ``
     tweetsData.forEach(function(tweet){
@@ -60,37 +67,58 @@ function getFeedHtml(){
             retweetIconClass = 'retweeted'
         }
 
+        let repliesHtml = ''
+        
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml += `
+                <div class="tweet-reply">
+                    <div class="tweet-inner">
+                        <img src="${reply.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${reply.handle}</p>
+                                <p class="tweet-text">${reply.tweetText}</p>
+                            </div>
+                    </div>
+                </div>
+            `
+            })
+        }
+
         feedHtml += `
-    <div class="tweet">
-        <div class="tweet-inner">
-            <img src="${tweet.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${tweet.handle}</p>
-                <p class="tweet-text">${tweet.tweetText}</p>
-                <div class="tweet-details">
-                    <span class="tweet-detail">
-                    <i class="fa-regular fa-comment-dots"
-                    data-reply="${tweet.uuid}"
-                    ></i>
-                        ${tweet.replies.length}
-                    </span>
-                    <span class="tweet-detail">
-                    <i class="fa-solid fa-heart ${likeIconClass}"
-                    data-like="${tweet.uuid}"
-                    ></i>
-                        ${tweet.likes}
-                    </span>
-                    <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet ${retweetIconClass}"
-                    data-retweet="${tweet.uuid}"
-                    ></i>                        
-                        ${tweet.retweets}
-                    </span>
-                </div>   
-            </div>            
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                        <i class="fa-regular fa-comment-dots"
+                        data-reply="${tweet.uuid}"
+                        ></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                        <i class="fa-solid fa-heart ${likeIconClass}"
+                        data-like="${tweet.uuid}"
+                        ></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet ${retweetIconClass}"
+                        data-retweet="${tweet.uuid}"
+                        ></i>                        
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+            <div class="hidden" id="replies-${tweet.uuid}">
+                ${repliesHtml}
+            </div>
         </div>
-    </div>
-    `
+        `
        })
        return feedHtml 
     }
